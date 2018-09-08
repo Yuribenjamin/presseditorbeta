831950d81a2e99e73365069347c1d91f4c7dbbe8
AbrahamRagab <Abrahammoustafa@hotmail.co.uk>
2018-09-08 15:33:51 +0200

    Create Header Component

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+import Header from './Header'
 
 
 class MainPage extends Component {
     render() {
-        return{
-
-        }
+        return(
+            <Header/>
+        )
     }
 }
 
