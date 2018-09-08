35f2fbb900d0bec61df7a27d4b84e06c4731b8f7
AbrahamRagab <Abrahammoustafa@hotmail.co.uk>
2018-09-08 15:23:29 +0200

    Create MainPage Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import MainPage from './MainPage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+     <main>
+       <MainPage/>
+     </main>
     );
   }
 }
