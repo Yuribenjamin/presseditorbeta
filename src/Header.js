6a5b393d7f8bdbfd64185ccf01e988079cdf7f34
AbrahamRagab <Abrahammoustafa@hotmail.co.uk>
2018-09-08 16:46:05 +0200

    Initialize header & add logo

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import global from './global.svg'
 const Header = () => (
-    <div>
-        <h1>Hello World!</h1>
+    <div className='App'>
+        <header className='App-header'>
+            <h1 className='App-title'>Press Editor</h1>
+            {/* Logo from https://www.flaticon.com/ */}
+            <img src={global} id='App-logo' alt='App-Logo'/>
+        </header>
     </div>
 );
 
