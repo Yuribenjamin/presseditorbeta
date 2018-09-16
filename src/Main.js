11f5b037f787fb9701770cb01aae084afe563d01
AbrahamRagab <Abrahammoustafa@hotmail.co.uk>
2018-09-16 18:35:30 +0200

    delete btn

--- src/Main.js
+++ src/Main.js
@@ -3,46 +3,11 @@ import React, { Component } from  'react'
 
 class Main extends Component {
 
-    saveText = () => {
-        
-        const textToWrite = document.getElementById('textarea').innerHTML;
-        let textFileAsBlob = new Blob([textToWrite], {type: 'text/plain'});
-        const fileNameToSaveAs = 'text.docx';
-
-        let downloadLink = document.createElement('a');
-        downloadLink.download = fileNameToSaveAs;
-        downloadLink.innerHTML = 'Download File';
-        if (window.webKitURL != null) {
-            window.download.href = window.webKitURL.createObjectURL(textFileAsBlob);
-        } else {
-            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-            downloadLink.onclick = window.destroyClickedElement;
-            downloadLink.style.display = 'none';
-            document.body.appendChild(downloadLink);
-        }
-        downloadLink.click();
-    }
-    
-    destroyClickedElement = (event) => {
-        document.body.removeChild(event.target);
-    }
-    
-
     render() {
        
         
         return (
             <main className='main'>
-                <div className='tools'>
-                <button
-                type='button'
-                onClick={() => this.saveText()}
-                value='Save'
-                id='save'
-                >
-                Save
-                </button>
-                </div>
                 <textarea 
                 id="textarea"
                 className='textarea-en' 
